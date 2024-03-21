ffa10591236fb708a2916cdd8ce9735c4fd170e0
SrijonCodeist <157360186+SrijonCodeist@users.noreply.github.com>
2024-03-21 10:45:06 +0530

    Create InputFunction.py

---
+++ InputFunction.py
@@ -0,0 +1,8 @@
+# Taking user input
+a = input("Enter your name: ")
+print("My name is", a)
+
+x = input("Enter First No.: ")
+y = input("Enter Second No.:")
+print(x + y)   # Wrong code as input function takes input as string data type 
+print(int(x) + int(y))  # Right code
