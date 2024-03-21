32896150736f11f86145c48873eade49c020bab7
SrijonCodeist <157360186+SrijonCodeist@users.noreply.github.com>
2024-03-21 10:12:19 +0530

    Create typecating.py

---
+++ typecating.py
@@ -0,0 +1,10 @@
+# typecasting 101: when we have to change any data type into other data type is known as Typecasting
+# python supports a wide variety of functions or methods like int(),float(),str(),ord(),hex(),set(),etc.
+a = "1"
+b = "2"
+print(int(a) + int(b))
+
+# Implicit Typecasting
+c = 2.9
+d = 8
+print(c + d)  # Changed the integer data type "8" to float data type which is a higher order data type automatically
