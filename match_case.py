26bf60c8229b3c6ed11cd476e89284f41417d363
SrijonCodeist <157360186+SrijonCodeist@users.noreply.github.com>
2024-03-27 13:06:43 +0530

    Create match_case.py

---
+++ match_case.py
@@ -0,0 +1,17 @@
+# Match Case Only runs in python version 3.10 or higher
+
+x = int(input("Enter the valur of x:"))
+# x is the variable to match
+match x:
+    # if x is 0
+    case 0:
+        print("x is zero")
+        # case with if-condition
+    case 4:
+        print("case is 4")
+    case _ if x != 90:
+        print(x, "is not 90")
+    case _ if x != 80:
+        print(x, "is not 80")
+    case _:
+        print(x)
